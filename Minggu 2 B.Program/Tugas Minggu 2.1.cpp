bae39876e8d486d5a53ae211e829d884ce1981c1
justjhnd <91574382+justjhnd@users.noreply.github.com>
2022-01-20 16:55:04 +0700

    Add files via upload

---
+++ Minggu 2 B.Program/Tugas Minggu 2.1.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+//Tugas Minggu 2 menghitung luas persegi panjang dengan type data double 
+using namespace std;
+main (){
+	cout << "\tAbdul Aziz Setiawan\n\t20200801072\n\tBahasa pemrograman\n\tTugas Minggu 2\n\n";
+	cout<< "Menghitung Luas persegi Panjang\n\n";
+	double panjang,lebar,luas;
+	cout<< " \n Masukan Nilai Panjang persegi = ";
+	cin >> panjang;
+	cout << "\n Masukan Nilai Lebar = ";
+	cin >> lebar;
+	luas = panjang * lebar ;
+	cout << "\n Luas Panjang Persegi =  " << luas <<endl ; 
+}
+
---
+++ Minggu 2 B.Program/Tugas minggu 2.2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "\tAbdul Aziz Setiawan\n\t20200801072\n\tBahasa pemrograman\n\tTugas Minggu 2\n\n";
+	int bil;
+	cout<<"Masukan Bilangan : ";
+	cin>>bil;
+	cout<<endl;
+
+	if (bil%2==0){
+		cout<<bil<<" Adalah bilangan genap"<<endl;
+	}else {
+		cout<<bil<<" Adalah bilangan ganjil"<<endl;	
+	}
+	cout << "terima kasih ";
+}
